52b18b2e4171c8d961875dd47dd11ec368c36693
quibbler01 <fluent0418@gmail.com>
2023-06-22 08:30:26 +0800

    demo ActivityResultContract

--- app/src/main/java/cn/quibbler/coroutine/MainActivity.kt
+++ app/src/main/java/cn/quibbler/coroutine/MainActivity.kt
@@ -1,7 +1,14 @@
 package cn.quibbler.coroutine
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.lifecycleScope
 import cn.quibbler.coroutine.databinding.ActivityMainBinding
 import kotlinx.coroutines.MainScope
@@ -14,15 +21,31 @@ class MainActivity : AppCompatActivity() {
 
     private val mainScope by lazy { MainScope() }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         //use Coroutine in Lifecycle
-        lifecycleScope.launch{
+        lifecycleScope.launch {
             //todo
         }
+
+        val register: ActivityResultContract<Intent, ActivityResult> = ActivityResultContracts.StartActivityForResult()
+
+        val callback = object : ActivityResultCallback<ActivityResult> {
+            override fun onActivityResult(result: ActivityResult?) {
+                Log.d("TAG", "resultCode:${result?.resultCode}  data:${result?.data}")
+            }
+        }
+
+        val launcher: ActivityResultLauncher<Intent> = registerForActivityResult(register, callback)
+
+        val intent = Intent()
+
+        launcher.launch(intent)
+
     }
 
     override fun onDestroy() {
