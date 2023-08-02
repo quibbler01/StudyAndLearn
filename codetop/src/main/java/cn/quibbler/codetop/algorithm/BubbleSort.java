f7f6394544742d975b33c842b5f4ed044206f096
quibbler01 <fluent0418@gmail.com>
2023-08-02 21:52:14 +0800

    pratice Bubble sort

---
+++ codetop/src/main/java/cn/quibbler/codetop/algorithm/BubbleSort.java
@@ -0,0 +1,17 @@
+package cn.quibbler.codetop.algorithm;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; ++i) {
+            for (int j = 0; j < nums.length - 1 - j; ++j) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+}
