31e52139c51ded375455c6ef169fc935f5f90dcb
quibbler01 <fluent0418@gmail.com>
2023-08-05 12:53:13 +0800

    TreeNode

---
+++ codetop/src/main/java/cn/quibbler/codetop/TreeNode.java
@@ -0,0 +1,22 @@
+package cn.quibbler.codetop;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
