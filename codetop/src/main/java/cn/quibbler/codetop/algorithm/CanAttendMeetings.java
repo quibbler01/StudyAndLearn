9ce56cf45ddcdc2c75875d6d1fd83e798136b774
quibbler01 <fluent0418@gmail.com>
2023-08-07 23:01:39 +0800

    canAttendMeetings

---
+++ codetop/src/main/java/cn/quibbler/codetop/algorithm/CanAttendMeetings.java
@@ -0,0 +1,23 @@
+package cn.quibbler.codetop.algorithm;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CanAttendMeetings {
+
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+        for (int i = 0; i < intervals.length - 1; ++i) {
+            if (intervals[i][1] > intervals[i + 1][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
