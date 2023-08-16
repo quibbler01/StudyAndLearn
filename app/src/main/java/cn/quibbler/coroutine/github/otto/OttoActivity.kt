f09f500169d6f2fadc33d94f196f8746ea6c3ce0
quibbler01 <fluent0418@gmail.com>
2023-08-16 12:31:42 +0800

    Learn Otto

---
+++ app/src/main/java/cn/quibbler/coroutine/github/otto/OttoActivity.kt
@@ -0,0 +1,38 @@
+package cn.quibbler.coroutine.github.otto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cn.quibbler.coroutine.R
+import com.squareup.otto.Bus
+import com.squareup.otto.Subscribe
+
+class OttoActivity : AppCompatActivity() {
+
+    companion object {
+
+        val bus = Bus()
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otto)
+
+        bus.register(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        bus.unregister(this)
+    }
+
+    class Event {
+
+    }
+
+    @Subscribe
+    fun onResult(event: Event) {
+
+    }
+
+}
---
+++ app/src/main/java/cn/quibbler/coroutine/github/otto/TestOtto.java
@@ -0,0 +1,30 @@
+package cn.quibbler.coroutine.github.otto;
+
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
+import com.squareup.otto.ThreadEnforcer;
+
+public class TestOtto {
+
+    private Bus bus = new Bus();
+
+    public void fun() {
+        Bus bus = new Bus("Quibbler");
+
+        bus.register(this);
+
+        bus.post(new Event());
+
+        bus.unregister(this);
+    }
+
+    public static class Event {
+        //something
+    }
+
+    @Subscribe
+    public void onEvent(Event event) {
+        //todo
+    }
+
+}
