4c4902193aa1f10f894296d5c56b4ad365eabd0d
quibbler01 <fluent0418@gmail.com>
2023-11-04 22:01:04 +0800

    Repository

---
+++ app/src/main/java/cn/quibbler/coroutine/kotlin/coroutine/Repository.kt
@@ -0,0 +1,65 @@
+package cn.quibbler.coroutine.kotlin.coroutine
+
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class Repository {
+
+    val scope = CoroutineScope(Job() + Dispatchers.Main)
+
+    init {
+        val job1 = scope.launch {
+
+        }
+
+        val job = scope.launch(Dispatchers.Default + CoroutineName("BackgroundCoroutine")) {
+
+        }
+    }
+
+    suspend fun fetchData() {
+        get()
+    }
+
+    private suspend fun get() {
+        withContext(Dispatchers.IO) {
+
+        }
+    }
+
+    private suspend fun fetchTwoDoc() {
+        coroutineScope {
+            val s1 = async { fetchDoc() }
+            val s2 = async { fetchDoc() }
+
+            s1.await()
+            s2.await()
+        }
+    }
+
+    private suspend fun fetchTwoDocAll() {
+        coroutineScope {
+            val deferreds = listOf(
+                async { fetchDoc() },
+                async { fetchTwoDoc() }
+            )
+            deferreds.awaitAll()
+        }
+    }
+
+    private suspend fun fetchDoc(): String {
+        withContext(Dispatchers.Default) {
+            delay(1000)
+        }
+        return ""
+    }
+
+}
