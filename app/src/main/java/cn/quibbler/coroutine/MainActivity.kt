7cb3f00a9b3e3873ace2c56b1357241a500fdf99
quibbler01 <fluent0418@gmail.com>
2023-11-07 22:40:45 +0800

    MutableStateFlow

--- app/src/main/java/cn/quibbler/coroutine/MainActivity.kt
+++ app/src/main/java/cn/quibbler/coroutine/MainActivity.kt
@@ -10,12 +10,20 @@ import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import cn.quibbler.coroutine.databinding.ActivityMainBinding
+import cn.quibbler.coroutine.jitpack.viewmodel.SearchViewModel
+import cn.quibbler.coroutine.kotlin.flow.LeastNewsUiState
+import cn.quibbler.coroutine.kotlin.flow.StateFlowVM
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -25,10 +33,36 @@ class MainActivity : AppCompatActivity() {
 
     private val mainScope by lazy { MainScope() }
 
+    private val viewModel: SearchViewModel by viewModels()
+
+    private val stateFlowVM: StateFlowVM by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel.uiState.update { str ->
+            "str$str"
+        }
+
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                stateFlowVM.uiState.collect {
+                    when (it) {
+                        is LeastNewsUiState.Success -> {
+                            val newsList = it.news
+
+                        }
+
+                        is LeastNewsUiState.Error -> {
+
+                        }
+                    }
+                }
+            }
+        }
 
         //use Coroutine in Lifecycle
         lifecycleScope.launch {
