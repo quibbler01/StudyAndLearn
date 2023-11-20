47b2debffcf3cc3108f3588157fa13e9900e1f4f
quibbler01 <fluent0418@gmail.com>
2023-11-20 23:04:55 +0800

    org.koin

---
+++ app/src/main/java/cn/quibbler/coroutine/App.kt
@@ -0,0 +1,27 @@
+package cn.quibbler.coroutine
+
+import android.app.Application
+import android.content.Context
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+
+            androidLogger(if (BuildConfig.DEBUG) Level.DEBUG else Level.NONE)
+
+            module {
+                single { this@App } bind Context::class
+            }
+        }
+    }
+
+}
