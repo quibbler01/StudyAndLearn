1430b0a6320c9efe4d5cc4b1054cabab2bb9cb19
quibbler01 <fluent0418@gmail.com>
2023-12-01 00:12:17 +0800

    kotlin by

--- app/src/main/java/cn/quibbler/coroutine/kotlin/byby/IProxy.kt
+++ app/src/main/java/cn/quibbler/coroutine/kotlin/byby/IProxy.kt
@@ -1,4 +1,15 @@
 package cn.quibbler.coroutine.kotlin.byby
 
 interface IProxy {
+
+    fun handle()
+
 }
+
+class ProxyImp : IProxy {
+    override fun handle() {
+    }
+}
+
+
+class ProxyBase : IProxy by ProxyImp()
