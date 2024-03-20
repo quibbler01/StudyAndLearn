b54e6a2dcaf61f4f3ff305d30d0ea5cea6f828b6
quibbler01 <fluent0418@gmail.com>
2024-03-20 21:18:03 +0800

    A cache that uses a bounded amount of space on a filesystem.

--- app/src/main/java/cn/quibbler/coroutine/ui/view/DiskLruCacheActivity.kt
+++ app/src/main/java/cn/quibbler/coroutine/ui/view/DiskLruCacheActivity.kt
@@ -1,8 +1,16 @@
 package cn.quibbler.coroutine.ui.view
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import cn.quibbler.coroutine.databinding.ActivityDiskLruCacheBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
+import org.json.JSONObject
 import java.io.File
 
 /**
@@ -12,6 +20,14 @@ import java.io.File
  */
 class DiskLruCacheActivity : AppCompatActivity() {
 
+    companion object {
+        const val TAG = "TAG_DiskLruCacheActivity"
+
+        const val key = "quibbler"
+
+        const val imageUrl = "https://so1.360tres.com/dr/270_500_/t012ff798d83b269e1c.jpg"
+    }
+
     private lateinit var binding: ActivityDiskLruCacheBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +36,57 @@ class DiskLruCacheActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initDiskLruCache()
+
+        val response: String = ""
+
+        if (response.isNotBlank()) {
+            val jsonObject = JSONObject(response)
+        }
+
+        saveBitmap()
+        getBitmap()
     }
 
+    private var diskLruCache: DiskLruCache? = null
+
     private fun initDiskLruCache() {
-        val diskLruCache = DiskLruCache.open(File(""), 1, 1, 1L)
+        diskLruCache = DiskLruCache.open(cacheDir, 1, 1, 1024 * 1024 * 24L)
+        Log.d(TAG, "diskLruCache $diskLruCache")
+    }
+
+    private fun saveBitmap() {
+        binding.saveBitmap.setOnClickListener {
+            Glide.with(this@DiskLruCacheActivity)
+                .asBitmap()
+                .load(imageUrl)
+                .into(object : SimpleTarget<Bitmap>() {
+                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                        val editor = diskLruCache?.edit(key)
+                        Log.d(TAG, "editor $editor")
+                        val outputStream = editor?.newOutputStream(0)
+                        Log.d(TAG, "outputStream $outputStream")
+                        outputStream?.let {
+                            resource.compress(Bitmap.CompressFormat.JPEG, 100, it)
+                            it.close()
+                        }
+                        editor?.commit()
+                    }
+                })
+        }
+    }
+
+    private fun getBitmap() {
+        binding.getBitmap.setOnClickListener {
+            val snapShot = diskLruCache?.get(key)
+            Log.d(TAG, "snapshot $snapShot")
+            val inputStream = snapShot?.getInputStream(0)
+            Log.d(TAG, "inputStream $inputStream")
+            inputStream?.let {
+                val bitmap = BitmapFactory.decodeStream(it)
+                Log.d(TAG, "bitmap $bitmap")
+                binding.image.setImageBitmap(bitmap)
+            }
+        }
     }
 
 }
