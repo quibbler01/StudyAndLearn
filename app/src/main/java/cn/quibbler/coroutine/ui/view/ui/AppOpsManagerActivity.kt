51b6cc2eaca95b551d933a3c80b5e1ac164f8e27
quibbler01 <fluent0418@gmail.com>
2024-06-29 22:16:37 +0800

    AppOpsManager

---
+++ app/src/main/java/cn/quibbler/coroutine/ui/view/ui/AppOpsManagerActivity.kt
@@ -0,0 +1,81 @@
+package cn.quibbler.coroutine.ui.view.ui
+
+import android.app.AppOpsManager
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import cn.quibbler.coroutine.R
+
+@RequiresApi(Build.VERSION_CODES.Q)
+class AppOpsManagerActivity : AppCompatActivity() {
+
+    private lateinit var appOpsManager: AppOpsManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_app_ops_manager)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        appOpsManager = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+
+        appOpsManager.startWatchingMode("", packageName, wacth)
+
+        ops()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        appOpsManager.stopWatchingMode(wacth)
+
+    }
+
+    val wacth = object : AppOpsManager.OnOpChangedListener {
+        override fun onOpChanged(op: String, packageName: String) {
+            val uid: Int = packageManager.getPackageUid(packageName, 0)
+            val result = appOpsManager.checkOpNoThrow(op, uid, packageName)
+            Log.e("zh", "权限发生变更 op = ${op}  packageName = ${packageName} ${result}")
+        }
+    }
+
+    private fun ops() {
+        appOpsManager
+    }
+
+    fun checkPermission(pkg: String, op: String): Int {
+        return try {
+            val uid = packageManager.getPackageUid(pkg, 0)
+            appOpsManager.checkOp(op, uid, pkg)
+            appOpsManager.unsafeCheckOp(op, uid, pkg)
+        } catch (_: Exception) {
+            -1
+        }
+    }
+
+    fun safeCheckPermission(pkg: String, op: String): Int {
+
+        val uid = packageManager.getPackageUid(pkg, 0)
+        appOpsManager.checkOpNoThrow(op, uid, pkg)
+        return appOpsManager.unsafeCheckOpNoThrow(op, uid, pkg)
+    }
+
+    fun checkUid(pkg: String, uid: Int): Boolean {
+        return runCatching {
+            appOpsManager.checkPackage(uid, pkg)
+
+            packageManager.getPackageUid(pkg, 0) == uid
+        }.isSuccess
+
+    }
+
+}
