22c1ea26b39941cf82b2e766dde2377ed03b10e6
quibbler01 <fluent0418@gmail.com>
2024-07-17 21:42:23 +0800

    two method to create Coroutine

---
+++ app/src/main/java/cn/quibbler/coroutine/kotlin/coroutine/Cor.kt
@@ -0,0 +1,46 @@
+package cn.quibbler.coroutine.kotlin.coroutine
+
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import okhttp3.Dispatcher
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.createCoroutine
+import kotlin.coroutines.resume
+import kotlin.coroutines.startCoroutine
+
+
+fun create() {
+
+
+    // result is Continuation<Unit>
+    val s = suspend {
+        1   // result
+    }.createCoroutine(object : Continuation<Int> {
+        override val context: CoroutineContext
+            get() = Dispatchers.IO
+
+        override fun resumeWith(result: Result<Int>) {
+            Log.d("QUIBBLER", "createCoroutine result:${result.getOrNull()}")
+        }
+    })
+
+    //start this Coroutine
+    s.resume(Unit)
+
+
+    //s1 is Unit type
+    val s1 = suspend {
+        99
+    }.startCoroutine(object : Continuation<Int> {
+        override val context: CoroutineContext
+            get() = EmptyCoroutineContext
+
+        override fun resumeWith(result: Result<Int>) {
+            Log.d("QUIBBLER", "startCoroutine result:${result.getOrNull()}")
+        }
+    })
+
+
+}
