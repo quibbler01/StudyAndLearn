34326739fdf878691923a81ebbc59b5b181a9e37
quibbler01 <fluent0418@gmail.com>
2024-07-30 00:07:59 +0800

    androidx.exifinterface.media.ExifInterface

---
+++ app/src/main/java/cn/quibbler/coroutine/ui/view/ui/ExifInterfaceActivity.kt
@@ -0,0 +1,38 @@
+package cn.quibbler.coroutine.ui.view.ui
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.exifinterface.media.ExifInterface
+import cn.quibbler.coroutine.R
+
+@RequiresApi(Build.VERSION_CODES.O)
+class ExifInterfaceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_exif_interface)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        test()
+    }
+
+    private fun test(){
+        val path = ""
+        val exif = ExifInterface("").apply {
+            thumbnailBitmap
+        }
+
+
+    }
+
+}
