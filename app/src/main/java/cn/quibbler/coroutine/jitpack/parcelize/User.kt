c365ff3e049d8deb83c097e317c751f2b8bf40e2
quibbler01 <fluent0418@gmail.com>
2024-09-14 20:09:43 +0800

    * https://mp.weixin.qq.com/s?__biz=MzA5MzI3NjE2MA==&mid=2650255228&idx=1&sn=f73e49c6934d9e235e0692a2452f21cd&chksm=88635e13bf14d705c5a9a2c49b1aeac24abeb7fa38206afb089043f9869f099da982841f8bba&scene=27

--- app/src/main/java/cn/quibbler/coroutine/jitpack/parcelize/User.kt
+++ app/src/main/java/cn/quibbler/coroutine/jitpack/parcelize/User.kt
@@ -2,10 +2,20 @@ package cn.quibbler.coroutine.jitpack.parcelize
 
 import android.os.Parcel
 import android.os.Parcelable
+import androidx.room.Entity
 import kotlinx.parcelize.Parceler
 import kotlinx.parcelize.Parcelize
 import kotlinx.parcelize.WriteWith
 
+/**
+ * https://developer.android.google.cn/training/data-storage/room?hl=zh-cn
+ * @property firstName String
+ * @property lastName String
+ * @property age Int
+ * @property externalClass [@kotlinx.parcelize.WriteWith<cn.quibbler.coroutine.jitpack.parcelize.ExternalClassParceler>] ExternalClass
+ * @constructor
+ */
+@Entity(tableName = "users")
 @Parcelize
 data class User(
     val firstName: String,
