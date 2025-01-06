8d9b899c4d439dd9b514263de45c3806796914c6
quibbler01 <fluent0418@gmail.com>
2025-01-06 22:24:38 +0800

    DiagnosisActivity

---
+++ app/src/main/java/cn/quibbler/coroutine/ui/view/ui/faker/DiagnosisActivity.kt
@@ -0,0 +1,23 @@
+package cn.quibbler.coroutine.ui.view.ui.faker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import cn.quibbler.coroutine.R
+
+class DiagnosisActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_diagnosis)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
