ca187fba6b146ad61c20a66cbb94e83bfff2e7a5
quibbler01 <fluent0418@gmail.com>
2025-11-05 21:31:19 +0800

    Multiple coroutine concurrent access can create multiple VMs, and the primary thread access must trigger initialization

--- app/src/main/java/cn/quibbler/coroutine/kotlin/flow/FlowActivity.kt
+++ app/src/main/java/cn/quibbler/coroutine/kotlin/flow/FlowActivity.kt
@@ -3,12 +3,15 @@ package cn.quibbler.coroutine.kotlin.flow
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.lifecycleScope
 import cn.quibbler.coroutine.databinding.ActivityFlowBinding
 import com.tencent.mmkv.MMKV
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -21,10 +24,23 @@ import kotlinx.coroutines.withContext
 
 class FlowActivity : AppCompatActivity() {
 
+    companion object{
+        const val TAG = "TAG_FlowActivity"
+    }
+
     private lateinit var binding: ActivityFlowBinding
 
     private val _flow: MutableStateFlow<String?> = MutableStateFlow(null)
     val flow: StateFlow<String?> = _flow.asStateFlow()
+
+    /**
+     * 不支持多线程并发访问，只支持主线程安全
+     *
+     * 多个协程并发访问可能创建多个VM，必须主线程访问触发初始化
+     */
+    private val stateVM: StateFlowVM by viewModels<StateFlowVM>()
+
+    val defaultScope = CoroutineScope(Dispatchers.Default)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,9 +49,41 @@ class FlowActivity : AppCompatActivity() {
 
         initView()
         lifecycleScope.launch {
-            MMKV.defaultMMKV().putString("key","hello")
+            MMKV.defaultMMKV().putString("key", "hello")
             delay(10000)
-            binding.console.append("${MMKV.defaultMMKV().getString("key","default")}")
+            binding.console.append("${MMKV.defaultMMKV().getString("key", "default")}")
+        }
+
+        stateVM.uiState
+
+        lifecycleScope.launch {
+            lifecycleScope.launch  {
+//                delay(100)
+                stateVM.uiState.collect {
+                    Log.d(TAG,"111        $it".apply { console(this) })
+                }
+            }
+
+            lifecycleScope.launch {
+//                delay(10)
+                stateVM.uiState.collect {
+                    Log.d(TAG,"222        $it".apply { console(this) })
+                }
+            }
+
+            defaultScope.launch(Dispatchers.Default)  {
+                delay(50)
+                stateVM.uiState.collect {
+                    Log.d(TAG,"333        $it".apply { console(this) })
+                }
+            }
+
+        }
+    }
+
+    private fun console(msg: String) {
+        lifecycleScope.launch(Dispatchers.Main) {
+            binding.console.append("$msg\n")
         }
     }
 
@@ -64,22 +112,13 @@ class FlowActivity : AppCompatActivity() {
             }
         )
 
-        lifecycleScope.launch(Dispatchers.IO){
+        lifecycleScope.launch(Dispatchers.IO) {
             var i = 0
             while (true) {
                 delay(100)
                 _flow.emit("${i++}")
             }
         }
-
-        lifecycleScope.launch {
-            flow.collect {
-                withContext(Dispatchers.Main) {
-                    binding.console.text = "$it"
-                }
-            }
-        }
-
     }
 
 }
--- app/src/main/java/cn/quibbler/coroutine/kotlin/flow/StateFlowVM.kt
+++ app/src/main/java/cn/quibbler/coroutine/kotlin/flow/StateFlowVM.kt
@@ -3,21 +3,27 @@ package cn.quibbler.coroutine.kotlin.flow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
 class StateFlowVM : ViewModel() {
 
-    private val _uiState = MutableStateFlow(LeastNewsUiState.Success(emptyList()))
-    val uiState: StateFlow<LeastNewsUiState> = _uiState
+    private val _uiState = MutableSharedFlow<Int>(replay = 1, extraBufferCapacity = 32)
+    val uiState: SharedFlow<Int> = _uiState.asSharedFlow()
 
     init {
         viewModelScope.launch {
             delay(2000)
-            _uiState.emit(LeastNewsUiState.Success(listOf("News One")))
+            repeat(5) {
+                _uiState.emit(it)
+            }
         }
     }
 
