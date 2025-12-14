30a6180d651964caf5fd29e351878b5738c69117
quibbler01 <fluent0418@gmail.com>
2025-12-14 09:22:56 +0800

    dynamicFeatures = [':dynamicfeature-downloader']

--- app/src/main/java/cn/quibbler/coroutine/jitpack/recyclerview/RecyclerViewActivity.kt
+++ app/src/main/java/cn/quibbler/coroutine/jitpack/recyclerview/RecyclerViewActivity.kt
@@ -1,13 +1,15 @@
 package cn.quibbler.coroutine.jitpack.recyclerview
 
+import android.app.ActivityOptions
+import android.content.Intent
+import android.graphics.Rect
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.ConcatAdapter
-import cn.quibbler.coroutine.R
 import cn.quibbler.coroutine.databinding.ActivityRecyclerViewBinding
+
 
 class RecyclerViewActivity : AppCompatActivity() {
 
@@ -27,9 +29,51 @@ class RecyclerViewActivity : AppCompatActivity() {
 
     private fun initVie() {
         binding.recyclerView.adapter = ConcatAdapter().apply {
-            repeat(5){
+            repeat(5) {
                 addAdapter(FoldAdapter())
             }
+        }
+        binding.jumpSmallWindow.setOnClickListener { view ->
+            val intent = getPackageManager().getLaunchIntentForPackage("com.mmbox.xbrowser.pro")!!
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+            val options: ActivityOptions = ActivityOptions.makeBasic()
+            options.setLaunchBounds(Rect(100, 100, 600, 800))
+            val bundle = options.toBundle()
+            bundle.putInt("android.activity.launchWindowingMode", 5)
+
+            runCatching {
+                val setLaunchWindowingMode =
+                    options.javaClass.getDeclaredMethod("setLaunchWindowingMode", Int::class.java)
+                        .apply {
+                            isAccessible = true
+                        }
+                setLaunchWindowingMode(options, 5)
+            }.onFailure {
+                Log.e(TAG, "setLaunchWindowingMode", it)
+            }
+            runCatching {
+                val mLaunchWindowingMode =
+                    options.javaClass.getDeclaredField("mLaunchWindowingMode")
+                        .apply {
+                            isAccessible = true
+                        }
+                mLaunchWindowingMode.set(options, 5)
+            }.onFailure {
+                Log.e(TAG, "mLaunchWindowingMode", it)
+            }
+            runCatching {
+                val mMiuiWindowingMode =
+                    options.javaClass.getDeclaredField("mMiuiWindowingMode")
+                        .apply {
+                            isAccessible = true
+                        }
+                mMiuiWindowingMode.set(options, 5)
+            }.onFailure {
+                Log.e(TAG, "mMiuiWindowingMode", it)
+            }
+
+            startActivity(intent, bundle)
         }
     }
 
